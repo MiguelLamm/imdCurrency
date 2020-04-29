1fddea8a7c5e1a7c1bfb05b7ccb3232ee48833dc
Miguel Lammens <justin.bugtong@gmail.com>
2020-04-29 16:10:20 +0200

    get all transactions test

--- sources/js/getSaldo.js
+++ sources/js/getSaldo.js
@@ -1,11 +1,24 @@
-fetch("http://localhost:3000/api/v1/transfer",{
+
+if (!localStorage.getItem("token")) {
+    window.location.href = "login.html";
+}
+console.log("goed2");
+fetch("http://localhost:3000/api/v1/transfer", {
     'headers': {
-        'Authorization':'Bearer '+ localStorage.getItem('token')
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
     }
-}).then(result =>{
+}).then(result => {
     return result.json();
+    
 }).then(json => {
-    console.log(json);
-}).catch(err => {
-    console.log("error");
-});
+    json.data.transfers.forEach(transfer => {
+
+console.log(json.data.transfers.to);
+            let newTransfer = ` <li>
+            <p class="list-naam">${json.data.transfers['2'].to}</p>
+            <p class="list-bedrag">€ ${json.data.transfers['2'].amount}</p>
+        </li>`;
+      
+        document.querySelector(".mainlist-history ").insertAdjacentHTML('beforeend', newTransfer);
+    })});
+    

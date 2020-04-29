32b718db746f9662789313a06870b05ef95b3d5f
Miguel Lammens <justin.bugtong@gmail.com>
2020-04-30 00:07:30 +0200

    foutmelding toegevoegd en extra beveiliging bij signup

--- sources/js/signup.js
+++ sources/js/signup.js
@@ -1,34 +1,48 @@
-var btnSignup = document.querySelector('.btn--signup').addEventListener("click", ()=>{
-    let userFname = document.querySelector('#signup-voornaam').value;
-    let userLname = document.querySelector('#signup-naam').value;
+var btnSignup = document.querySelector('.btn--signup').addEventListener("click", () => {
+
     let email = document.querySelector('#signup-email').value;
-    let password = document.querySelector('#signup-ww').value;
-    let username = userFname + " "+userLname;
-    console.log(username);
+    const substring = "@student.thomasmore.be";
+    if (email.includes(substring) == true) {
 
+        let userFname = document.querySelector('#signup-voornaam').value;
+        let userLname = document.querySelector('#signup-naam').value;
 
-   fetch('http://localhost:3000/users/signup', {
-        method: "post",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "username": username,
-            "email": email,
-            "password": password
-        })
-    }).then(response => {
-        return response.json();
-        
-    }).then(json => {
-        if (json.status == "success"){
-            let token = json.data.token;
-            localStorage.setItem("token", token);
-            window.location.href= "main.html";
+        let password = document.querySelector('#signup-ww').value;
+        let passlength = password.length;
+        console.log(passlength);
+        let username = userFname + " " + userLname;
+        console.log(username);
+
+        if (passlength > 3) {
+            console.log("processing");
+            fetch('http://localhost:3000/users/signup', {
+                method: "post",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "username": username,
+                    "email": email,
+                    "password": password
+                })
+            }).then(response => {
+                return response.json();
+
+            }).then(json => {
+                if (json.status == "success") {
+                    let token = json.data.token;
+                    localStorage.setItem("token", token);
+                    window.location.href = "main.html";
+                } else if (json.status == "error") {
+                    alert('error');
+                }
+            })
+        } else {
+            document.querySelector(".hidden").className = "show";
         }
 
-        else if (json.status == "error"){
-            alert('error');
-         }
-    })
+
+    } else {
+        document.querySelector(".hidden").className = "show";
+    }
 });

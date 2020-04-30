dec1c7a03100fae2b75cb3d6d8ec02e062b91257
Miguel Lammens <justin.bugtong@gmail.com>
2020-04-30 18:13:14 +0200

    gulpfile activeren

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
-function test() {
-    // place code for your default task here
-    console.log("hello");
-  }
-  
-  exports.test = test;
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+sass2css = function(){
+  return src('sources/sass/app.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(dest('dist/app.css'));
+}
+exports.default = function(){
+  watch("./sources/sass/**/*.scss", sass2css);
+};

33b58c2934f2f3de9d4378584fd5c8a895a2fc8a
Miguel Lammens <justin.bugtong@gmail.com>
2020-05-01 23:45:13 +0200

    textarea done

--- sources/js/createTrans.js
+++ sources/js/createTrans.js
@@ -17,7 +17,6 @@ fetch(base_url+ '/leaderboard', {
         let alledata = json.data;
         for (let i = 0; i <alledata.length ; i++) {
         users[i]=json.data[i].nickname;
-        console.log(users);
         let newItem = `<option>${json.data[i].nickname}</option>`;
     
                 document.querySelector("#namelist").insertAdjacentHTML('beforeend', newItem);
@@ -27,19 +26,54 @@ fetch(base_url+ '/leaderboard', {
         alert('error');
      }
 })
+document.getElementById('textarea').className= "textareahidden";
 
-
+let option = document.getElementById('transMess').addEventListener("click", ()=>{
+    options =  document.getElementById('transMess').value;
+    if(options == "11"){
+        document.getElementById('textarea').className= "textareashow";
+    } else {
+        document.getElementById('textarea').className= "textareahidden";
+    }
+})
 
 //TRANSACTIE VERSTUREN
 let btnTrans = document.querySelector('.btn--trans').addEventListener("click", ()=>{
     let to = document.querySelector('#transTo').value;
     let amount = document.querySelector('#transAmount').value;
+    let personalMess= document.querySelector('#textarea').value;
     let message = document.querySelector('#transMess').value;
 
     if(to){
         if(amount){
-            if(message){
+            if(message=="11"){
                 
+                fetch(base_url+ '/api/v1/transfer', {
+                    method: "post",
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Bearer ' + localStorage.getItem('token')
+                    },
+                    body: JSON.stringify({
+                        "to": to,
+                        "amount": amount,
+                        "message": personalMess
+                    })
+                }).then(response => {
+                    return response.json();
+                    
+                }).then(json => {
+                    if (json.status == "success"){
+                       
+                        window.location.href= "main.html";
+                    }
+            
+                    else if (json.status == "error"){
+                        alert('error');
+                     }
+                })
+            }
+            else if(message!="11"){
                 fetch(base_url+ '/api/v1/transfer', {
                     method: "post",
                     headers: {
@@ -64,8 +98,7 @@ let btnTrans = document.querySelector('.btn--trans').addEventListener("click", (
                         alert('error');
                      }
                 })
-            }
-            else {
+            }else{
                 document.querySelector(".hidden").className = "show";
             }
         }
